2a70e8316f5bf4b3c3ecbbfea50b9bcf28072137
paltyshev <aleksep@gmail.com>
2022-10-01 14:08:22 +0000

    translate root.tsx

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -47,12 +47,12 @@ export default function App() {
       navigator.serviceWorker.register("/sw.js").then(
         function (registration) {
           console.log(
-            "ServiceWorker registration successful with scope: ",
+            "Регистрация ServiceWorker прошла успешно с областью действия: ",
             registration.scope
           );
         },
         function (err) {
-          console.log("ServiceWorker registration failed: ", err);
+          console.log("Регистрация ServiceWorker не удалась: ", err);
         }
       );
     });
